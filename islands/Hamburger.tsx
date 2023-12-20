e226644581ebdc7e113fb3449a03104932915ceb
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-12-20 19:37:50 +1300

    add navbar text

--- islands/Hamburger.tsx
+++ islands/Hamburger.tsx
@@ -1,39 +1,50 @@
-import {useState} from "preact/hooks";
+import { useState } from "preact/hooks";
 
 export default function Hamburger() {
   const [isOpen, setIsOpen] = useState(false);
-  const genericHamburgerLine = `h-1 w-6 my-1 rounded-full bg-black transition ease transform duration-300`;
+  const genericHamburgerLine =
+    `h-1 w-6 my-1 rounded-full bg-black transition ease transform duration-300`;
 
   return (
-    <div className={`${
-      isOpen
-        ? "rounded-lg pt-4 bg-[#4f504f] h-screen sm:w-screen md:w-1/2 transition ease transform duration-300"
-        : "bg-[#b9bab9] pt-4"
-    }`}>
-    <button
-      className="ml-4 flex flex-col h-12 w-12 border-2 border-black rounded justify-center items-center group"
-      onClick={() => setIsOpen(!isOpen)}
+    <div
+      className={`float-left ${
+        isOpen
+          ? "pt-4 bg-[#4f504f] h-screen sm:w-screen md:w-1/2 transition ease transform duration-300"
+          : "bg-[#b9bab9] pt-4 md:relative"
+      }`}
     >
-      <div
-        className={`${genericHamburgerLine} ${
-          isOpen
-            ? "rotate-45 translate-y-3 opacity-50 group-hover:opacity-100"
-            : "opacity-50 group-hover:opacity-100"
-        }`}
-      />
-      <div
-        className={`${genericHamburgerLine} ${
-          isOpen ? "opacity-0" : "opacity-50 group-hover:opacity-100"
-        }`}
-      />
-      <div
-        className={`${genericHamburgerLine} ${
-          isOpen
-            ? "-rotate-45 -translate-y-3 opacity-50 group-hover:opacity-100"
-            : "opacity-50 group-hover:opacity-100"
-        }`}
-      />
-    </button>
+      <button
+        className="ml-4 flex flex-col h-12 w-12 border-2 border-black rounded justify-center items-center group"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div
+          className={`${genericHamburgerLine} ${
+            isOpen
+              ? "rotate-45 translate-y-3 opacity-50 group-hover:opacity-100"
+              : "opacity-50 group-hover:opacity-100"
+          }`}
+        />
+        <div
+          className={`${genericHamburgerLine} ${
+            isOpen ? "opacity-0" : "opacity-50 group-hover:opacity-100"
+          }`}
+        />
+        <div
+          className={`${genericHamburgerLine} ${
+            isOpen
+              ? "-rotate-45 -translate-y-3 opacity-50 group-hover:opacity-100"
+              : "opacity-50 group-hover:opacity-100"
+          }`}
+        />
+      </button>
+      <nav className={isOpen ? "float-right" : "hidden"}>
+        <ul>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Blog</li>
+          <li>Contact Me</li>
+        </ul>
+      </nav>
     </div>
   );
-};
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -3,10 +3,12 @@ import Hamburger from "../islands/Hamburger.tsx";
 export default function Home() {
   return (
     <div class="h-screen bg-[#b9bab9] overflow-hidden">
-    <Hamburger />
-      <div class="h-screen relative flex flex-col items-end justify-between">
+      <Hamburger />
+      <div class="float-right mt-10 h-screen relative flex flex-col items-end justify-between">
         <h2 class="text-4xl font pt-4 pr-4">my portfolio</h2>
-        <h1 class="text-8xl pb-4 pr-4 text-right font-bold">kirsty <br/> ammundsen</h1>
+        <h1 class="text-8xl pb-4 pr-4 text-right font-bold">
+          kirsty <br /> ammundsen
+        </h1>
         <img
           class="my-6 rounded-md"
           src="/KirstyPortfolio.jpg"
