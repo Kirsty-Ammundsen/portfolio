1aace39acca00927453aae5511f780e85d240df4
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-12-21 22:52:53 +1300

    update styling of boxes

--- routes/about.tsx
+++ routes/about.tsx
@@ -6,17 +6,14 @@ export default function About() {
       <Hamburger />
       <div class="flex justify-between">
       <div class="flex flex-col items-center justify-center">
-        <p class="bg-white text-xl py-6 px-4
-        rounded-lg opacity-60 ml-20 mr-28 font-cursive">'Hello, my name is Kirsty - I'm a tech person with a background in healthcare.
-        <br/>
-        <br/>
-        It would be ideal to work in a job where I could meld the two together, but as I'm only at the beginning of my tech journey, I'm still open-minded about all the possibilities available to me.  I want to learn, grow, and discover where I fit in the tech space.  As long as it involves working with people and life-long learning, I think it will be fun!
-        <br/>
-        <br/>
-        Apart from that, I love solving escape rooms, eating delicious food, and travelling the world. '
-        <br/>
-        <br/>
-        To find out more, please <a class="hover:text-green-800 font-bold" href="https://docs.google.com/document/d/1E1kWHfyMwnAf44LNHI8WtqTw7mKqbCDaPjlws0O73aA/export?format=pdf">download my resume.</a></p>
+        <div class="bg-white text-xl py-6 px-4
+        rounded-lg opacity-60 ml-20 mr-28 font-cursive">
+          <p class="py-2">'Hello, my name is Kirsty - I'm a tech person with a background in healthcare.</p>
+          <p class="py-2">It would be ideal to work in a job where I could meld the two together, but as I'm only at the beginning of my tech journey, I'm still open-minded about all the possibilities available to me.  I want to learn, grow, and discover where I fit in the tech space.  As long as it involves working with people and life-long learning, I think it will be fun!</p>
+          <p class="py-2">Apart from that, I love solving escape rooms, eating delicious food, and travelling the world. </p>
+          <p class="font-serif py-4">To find out more, please <a class="hover:text-green-800 font-bold" href="https://docs.google.com/document/d/1E1kWHfyMwnAf44LNHI8WtqTw7mKqbCDaPjlws0O73aA/export?format=pdf">download my resume.</a>
+          </p>
+        </div>
       </div>
       <div class="float-right mt-10 h-screen relative flex flex-col items-end justify-between">
         <h1 class="text-4xl pt-4 pr-4">about me</h1>
--- routes/contact.tsx
+++ routes/contact.tsx
@@ -4,30 +4,30 @@ export default function Contact() {
   return (
     <div class="h-screen bg-[#b9bab9] overflow-hidden">
       <Hamburger />
-      <div class="flex justify-between ">
-      <div class="flex flex-col items-center justify-center bg-white opacity-60">
-        <p class="text-2xl py-6 px-6">Resume - <a class="hover:text-green-800 font-bold" href="https://docs.google.com/document/d/1E1kWHfyMwnAf44LNHI8WtqTw7mKqbCDaPjlws0O73aA/export?format=pdf">download my resume</a>
-        <br/>
-        <br/>
-        LinkedIn - <a class="hover:text-green-800 font-bold" href="https://www.linkedin.com/in/kirsty-ammundsen">view my profile</a>
-        <br/>
-        <br/>
-        GitHub - <a class="hover:text-green-800 font-bold" href="https://github.com/Kirsty-Ammundsen">see my code</a>
-        </p>
-      </div>
-      <div class="float-right mt-10 h-screen relative flex flex-col items-end justify-between">
-        <h2 class="text-4xl pt-4 pr-4">contact info</h2>
-        <h1 class="text-8xl pb-4 pr-4 text-right font-bold">
-          kirsty <br /> ammundsen
-        </h1>
-        <img
-          class="my-6 rounded-md"
-          src="/KirstyPortfolio.jpg"
-          width="250"
-          height="200"
-          alt="Black and white portrait of Kirsty looking over her shoulder smiling"
-        />
-      </div>
+      <div class="flex justify-between">
+        <div class="flex flex-col items-center justify-center ">
+          <p class="bg-white opacity-60 rounded-lg text-2xl py-12 px-6 ml-20 ">Resume - <a class="hover:text-green-800 font-bold" href="https://docs.google.com/document/d/1E1kWHfyMwnAf44LNHI8WtqTw7mKqbCDaPjlws0O73aA/export?format=pdf">download my resume</a>
+          <br/>
+          <br/>
+          LinkedIn - <a class="hover:text-green-800 font-bold" href="https://www.linkedin.com/in/kirsty-ammundsen">view my profile</a>
+          <br/>
+          <br/>
+          GitHub - <a class="hover:text-green-800 font-bold" href="https://github.com/Kirsty-Ammundsen">see my code</a>
+          </p>
+        </div>
+        <div class="float-right mt-10 h-screen relative flex flex-col items-end justify-between">
+          <h2 class="text-4xl pt-4 pr-4">contact info</h2>
+          <h1 class="text-8xl pb-4 pr-4 text-right font-bold">
+            kirsty <br /> ammundsen
+          </h1>
+          <img
+            class="my-6 rounded-md"
+            src="/KirstyPortfolio.jpg"
+            width="250"
+            height="200"
+            alt="Black and white portrait of Kirsty looking over her shoulder smiling"
+          />
+        </div>
       </div>
     </div>
   );
