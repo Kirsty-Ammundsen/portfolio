6bf449fbeb0bcbf5ea4bf6d108401ec7aaaa68ec
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-12-23 10:33:28 +1300

    write patchboard project info

--- components/Tech.tsx
+++ components/Tech.tsx
@@ -8,7 +8,7 @@ export default function Tech(props: Props) {
 const {children} = props
 
   return (
-    <div className="px-3 py-2 border-gray-500 border-2 rounded-full bg-[#b9bab9] text-center text-black font-bold text-base">
+    <div className="px-2 py-1 border-gray-500 border-2 rounded-full bg-[#b9bab9] text-center text-black font-bold text-base">
       {children}
     </div>
   );
--- routes/projects.tsx
+++ routes/projects.tsx
@@ -8,22 +8,22 @@ export default function Projects() {
       <div class="flex justify-between">
         <div class="flex flex-col items-center justify-center">
           <main class="bg-offWhite text-2xl py-3 px-4 rounded-lg ml-20 mr-28 h-[80vh] my-20 overflow-y-scroll">
-            <section class="py-3">
+            <section class="py-5">
               <h3 class="text-2xl font-bold">Task Master</h3>
               <img
                 class="my-6 rounded-md"
                 src="/taskmaster.png"
                 // width="250"
                 // height="200"
-                alt="Black and white portrait of Kirsty looking over her shoulder smiling"
+                alt="Screenshots (mobile sized) of the taskmaster app layout, showing the log in, profile, tasks and stats pages."
               />
               <h4 class="text-lg pb-2">Description:</h4>
               <p class=" text-base">
                 A mobile app that allows physios to assign tasks to their
                 clients and track their progress as they are completed. I
                 created this app in a group of 4 people over 7 days. My main
-                contribution was doing the CSS (the layouts, components,
-                navigation bar, and login page) and creating the stats page from
+                contributions were: coming up with the idea, acting as project manager to decide the basic functions and scope of the project, doing the CSS (the layouts, components,
+                navigation bar, and login page), and creating the stats page from
                 the database queries, API calls, to implementing chartJS to make
                 the graphs.
               </p>
@@ -33,33 +33,28 @@ export default function Projects() {
                 <Tech>Tailwind CSS</Tech>
                 <Tech>React</Tech>
                 <Tech>Knex</Tech>
+                <Tech>SQLite3</Tech>
               </div>
             </section>
-            <section>
-              <h3 class="text-2xl font-bold">New Project</h3>
+            <section class="py-5">
+              <h3 class="text-2xl font-bold">PAtCHboaRD</h3>
               <img
                 class="my-6 rounded-md"
-                src="/taskmaster.png"
+                src="/patchboard.png"
                 // width="250"
                 // height="200"
-                alt="Black and white portrait of Kirsty looking over her shoulder smiling"
+                alt="Screenshot (laptop version) of the patchboard project with 4 sticky notes showing to do lists and an image carousel (image that rotates on click)."
               />
               <h4 class="text-lg pb-2">Description:</h4>
               <p class=" text-base">
-                A mobile app that allows physios to assign tasks to their
-                clients and track their progress as they are completed. I
-                created this app in a group of 4 people over 7 days. My main
-                contribution was doing the CSS (the layouts, components,
-                navigation bar, and login page) and creating the stats page from
-                the database queries, API calls, to implementing chartJS to make
-                the graphs.
+                Our first React project in a team of 4, done over 5 hours.  It is a website that allows you to write to do lists and store pictures on your screen. My main contribution was styling the components using vanilla CSS (which goes through multiple themes on clicking the background).
               </p>
               <h4 class="text-lg py-2">Tech Used:</h4>
               <div class="flex flex-row flex-wrap justify-center gap-2">
-                <Tech>TypeScript</Tech>
-                <Tech>Tailwind CSS</Tech>
                 <Tech>React</Tech>
-                <Tech>Knex</Tech>
+                <Tech>CSS</Tech>
+                <Tech>JavaScript</Tech>
+                <Tech>HTML</Tech>
               </div>
             </section>
           </main>
