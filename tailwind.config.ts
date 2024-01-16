b571746477e28432d024dc572f4de6c1899a13a4
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2024-01-17 10:42:16 +1300

    add smaller breakpoint for smaller phones

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,23 @@ export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
-  theme:{
-    fontFamily:{
-      'cursive': ['Diphylleia', 'serif']
+  theme: {
+    fontFamily: {
+      "cursive": ["Diphylleia", "serif"],
     },
-    extend:{
+    extend: {
       colors: {
-        'primary': '#b9bab9',
-        'darkGray': '#4f504f',
-        'lightGray': '#d6d7d6',
-        'offWhite':'#f2f2f2',
+        "primary": "#b9bab9",
+        "darkGray": "#4f504f",
+        "lightGray": "#d6d7d6",
+        "offWhite": "#f2f2f2",
       },
-    }
-  }
+      screens: {
+        "xs": "370px",
+      },
+      fontSize: {
+        "2.5xl": "2.6rem",
+      },
+    },
+  },
 } satisfies Config;
